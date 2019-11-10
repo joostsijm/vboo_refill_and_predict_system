99cf555781245e01e7e1995700e68cd136ce42f1
JoostSijm <joostsijm@gmail.com>
2019-11-10 23:27:02 +0100

    Remove database metadata

--- app/models.py
+++ app/models.py
@@ -2,19 +2,12 @@
 
 import datetime
 
-from sqlalchemy import MetaData, Column, ForeignKey, Integer, String, SmallInteger, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, SmallInteger, DateTime
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 
-meta = MetaData(naming_convention={
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s"
-})
-Base = declarative_base(metadata=meta)
+Base = declarative_base()
 
 class Region(Base):
     """Model for region"""
