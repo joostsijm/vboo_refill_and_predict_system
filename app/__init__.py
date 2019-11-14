0976914b97bb76545c38bd8705cda1e8f8b7a141
JoostSijm <joostsijm@gmail.com>
2019-11-14 15:38:46 +0100

    Add updated logger in init

--- app/__init__.py
+++ app/__init__.py
@@ -17,15 +17,30 @@ load_dotenv()
 ENGINE = create_engine(os.environ["DATABASE_URI"])
 SESSION = sessionmaker(bind=ENGINE)
 
-# scheduler
-SCHEDULER = BackgroundScheduler()
-SCHEDULER.start()
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+# get logger
 LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+SCHEDULER_LOGGER = logging.getLogger('apscheduler')
+SCHEDULER_LOGGER.setLevel(logging.DEBUG)
+
+# create file handler
+FILE_HANDLER = logging.FileHandler('irlt.log')
+FILE_HANDLER.setLevel(logging.DEBUG)
+
+# create console handler
+STREAM_HANDLER = logging.StreamHandler()
+STREAM_HANDLER.setLevel(logging.INFO)
+
+# create formatter and add it to the handlers
+FORMATTER = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+STREAM_HANDLER.setFormatter(FORMATTER)
+FILE_HANDLER.setFormatter(FORMATTER)
+
+# add the handlers to logger
+LOGGER.addHandler(STREAM_HANDLER)
+LOGGER.addHandler(FILE_HANDLER)
+SCHEDULER_LOGGER.addHandler(STREAM_HANDLER)
+SCHEDULER_LOGGER.addHandler(FILE_HANDLER)
 
 # api
 BASE_URL = os.environ["API_URL"]
